26a9d341af500b56205e87fef3e167d9b2bf449e
Rijan Maharjan <81901737+Rijan9860@users.noreply.github.com>
2022-05-26 23:28:49 +0545

    Cleaned Header and Footer

--- Restaurant/udibaba/urls.py
+++ Restaurant/udibaba/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from udibaba import views
+from . import views
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-     path('', views.home,name='home'),
-     path('cart/', views.cart, name='cart')
+     path('', views.home,name="home"),
+     path('cart/', views.cart, name='cart'),
+     path('contact/', views.contact, name='contact'),
+     path('about/', views.about, name='about'),
+     path('review/', views.review, name='review'),
+     path('menu/', views.menu, name='menu'),
 ]
--- Restaurant/udibaba/views.py
+++ Restaurant/udibaba/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
+# Create your views here.
 def home(request):
     return render(request,'index.html')
 
 def cart(request):
     return render(request, 'cart/cart.html')
+
+def contact(request):
+    return render(request, 'contact/contact.html')
+
+def about(request):
+    return render(request, 'aboutus/about.html')
+
+def review(request):
+    return render(request, 'review/review.html')
+
+def menu(request):
+    return render(request, 'menu/menu.html')
