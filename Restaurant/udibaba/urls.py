d9d898a0868b3f5e510ae1ea7b422b601cef10e8
bipul077 <81355345+bipul077@users.noreply.github.com>
2022-05-26 22:02:39 +0545

    Header update

--- Restaurant/udibaba/urls.py
+++ Restaurant/udibaba/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from udibaba import views
+from . import views
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-     path('', views.home,name='home'),
-     path('cart/', views.cart, name='cart')
+     path('', views.home,name="home")
 ]
