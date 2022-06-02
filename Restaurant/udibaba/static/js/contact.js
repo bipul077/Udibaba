2654825ab6b93f4673e09969afef25e91fd527fb
bipul077 <np03a190012@heraldcollege.edu.np>
2022-06-02 15:42:33 +0545

    menu pop

--- Restaurant/udibaba/static/js/contact.js
+++ Restaurant/udibaba/static/js/contact.js
@@ -1,24 +1,18 @@
-<<<<<<< HEAD
+
 document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.preventDefault();
     e.target.elements.name.value = '';
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
   });
-=======
-<<<<<<< HEAD
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
-=======
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
->>>>>>> 140c0d9e84bedf645a664cb672612cceaa0bfe5e
->>>>>>> e9415d988fa7118026f5749f562b74bf5ad67503
+$('.alink').click(function () {
+  console.log("clicked");
+  $('#modalbody').html($(this).data('name'));
+  // var ids = document.getElementById('imgid').src;
+  // var div = document.getElementById('modalbody');
+  // console.log(ids);
+  // console.log(div)
+  // var img = document.createElement("img");
+  // img.src = ids;
+  // div.appendChild(img);
+})
